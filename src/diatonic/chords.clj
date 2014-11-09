b836d56f10e12975a06b55cd6e67b6c8c4c90758
Ulrich Rhein <ulrich@rhein.name>
2014-11-09 19:30:50 +0100

    Added "context" framework and migrated chords.clj to it

--- src/diatonic/chords.clj
+++ src/diatonic/chords.clj
@@ -7,6 +7,7 @@
   (+ 3 (if (:base step) 1 0) (if (:chord step) 1 0)))
 
 (defn chords [base-scale len]
+  "Returns a seq of len scales, starting with base-scale."
   (let [diatonic-steps (s/diatonic-steps base-scale)
         step-weights (into {} (map (juxt identity step-weight)
                                    diatonic-steps))
@@ -15,16 +16,17 @@
                      (s/derived-scale base-scale (:step scale-step))))]
     (cons base-scale (take (dec len) (repeatedly chord-fn)))))
 
-(defn- inversion [scale]
-  (let [diatonics (s/diatonic-steps scale)
+(defn- inversion [chord]
+  (let [scale (:chord chord)
+        base-step (:base-step chord)
+        diatonics (s/diatonic-steps scale)
         base (first diatonics)
         third (nth diatonics 2)
-        fifth (nth diatonics 4)
-        type (g/weighted {:base 50 :inv-1 10 :inv-2 10})]
-    (condp = type
-      :base [base third fifth]
-      :inv-1 [(n/transpose -12 fifth) base third]
-      :inv-2 [third fifth (n/transpose 12 base)])))
+        fifth (nth diatonics 4)]
+    (condp = base-step
+      0 [base third fifth]
+      2 [(n/transpose -12 fifth) base third]
+      4 [third fifth (n/transpose 12 base)])))
 
 (defn- place-chord-near
   "Place chord (specified by a note vector) near to the specified
@@ -38,14 +40,39 @@
         transpose-by (- pitch-diff pitch-rem pitch-adjust)]
     (map (partial n/transpose transpose-by) chord)))
 
-(defn chord-notes [scale base-scale velocity offset length]
+(defn- chord-notes [chord base-scale velocity offset length]
   (map #(assoc % :velocity velocity :offset offset :length length)
-       (place-chord-near (s/base-pitch base-scale) (inversion scale))))
+       (place-chord-near (s/base-pitch base-scale) (inversion chord))))
 
-(defn chord-track [base-scale len chord-len]
-  (let [c (chords base-scale len)]
-    {:type :chord
-     :notes (mapcat
-             (fn [chord offset]
-               (chord-notes chord base-scale 127 offset chord-len))
-             c (range 0 (* len chord-len) chord-len))}))
+(defn init-chords
+  "Initializes the chord list of the specified context, if it's not
+  already present."
+  [context]
+  (if-not (:chords context)
+    (let [base-scale (:base-scale context)
+          chords (chords base-scale (:bars context))
+          inversion-weights {0 5, 2 1, 4 1}]
+      (assoc context :chords
+             (vec
+              (map-indexed
+               (fn [idx chord]
+                 {:chord chord
+                  :base-step (if (> idx 0)
+                               (g/weighted inversion-weights) 0)})
+               chords))))
+    context))
+
+(defn init-chord-notes
+  "Initializes the chord note list of the context, if it's not already
+  present."
+  [context]
+  (if-not (:chord-notes context)
+    (let [base-scale (:base-scale context)
+          chords (:chords context)
+          chord-len (:notes-per-bar context)]
+      (assoc context :chord-notes
+             (mapcat
+              (fn [chord offset]
+                (chord-notes chord base-scale 120 offset chord-len))
+              chords (range 0 (* (:bars context) chord-len) chord-len))))
+    context))
--- src/diatonic/composition.clj
+++ src/diatonic/composition.clj
@@ -6,11 +6,34 @@
             [diatonic.scales :as s]))
 
 (defmacro with-seed [seed & body]
-  "Creates a PRNG with the specified seed and binds it to be used in the body."
+  "Creates a PRNG with the specified seed and binds it to be used in
+  the body."
   `(binding [g/*rnd* (java.util.Random. ~seed)]
      (do ~@body)))
 
+(defn init-context
+  "Initializes an empty context with default values."
+  ([] (init-context {}))
+  ([context] (merge {:notes-per-bar 32 :bars 4} context)))
 
+(defn init-base-scale
+  "Initializes the base scale of the specified context, if it's not
+  already present."
+  [context]
+  (if (:base-scale context)
+    context
+    (assoc context :base-scale
+           ((if (g/boolean) s/ionic-scale s/aeolic-scale) 64))))
+
+(defn compose [seed]
+  (with-seed (if (string? seed) (.hashCode seed) seed)
+    (let [context (init-context {:seed seed})]
+      (-> context
+          (init-base-scale)
+          (c/init-chords)
+          (c/init-chord-notes)))))
+
+; (compose "1234")
 (defn play [seed]
   (with-seed (if (string? seed) (.hashCode seed) seed)
     (let [len 4
